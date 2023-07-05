c23a273abc7b41507681ecaac25806f6dd082b7b
zbz <nnzbz@21cn.com>
2023-07-05 23:12:50 +0800

    + Json打印dbMeta

--- codgen-core/src/main/java/com/github/codgen/core/Codgen.java
+++ codgen-core/src/main/java/com/github/codgen/core/Codgen.java
@@ -1,12 +1,14 @@
 package com.github.codgen.core;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import rebue.wheel.core.JdbcUtils;
+import rebue.wheel.serialization.jackson.JacksonUtils;
 
 import java.sql.SQLException;
 
 public class Codgen {
-    public static void gen(GenOptions options) throws SQLException {
+    public static void gen(GenOptions options) throws SQLException, JsonProcessingException {
         JdbcUtils.DbMeta dbMeta = JdbcUtils.getDbMeta(options.getJdbc().getConnect(), options.getJdbc().getTableName());
-        System.out.printf("database meta: %s%n", dbMeta);
+        System.out.printf("database meta: %s%n", JacksonUtils.serializeWithPretty(dbMeta));
     }
 }
